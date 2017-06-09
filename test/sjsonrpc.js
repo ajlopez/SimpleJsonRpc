1628f33ee16b1ac3ce0026fcd25cbf2a93f2d905
ajlopez <webmaster@ajlopez.com>
2017-06-09 14:54:53 -0300

    Call dummy provider

--- test/sjsonrpc.js
+++ test/sjsonrpc.js
@@ -16,3 +16,20 @@ exports['sjr create client with dummy provider'] = function (test) {
 	test.equal(typeof client.call, 'function');
 }
 
+exports['call dummy provider'] = function (test) {
+	test.async();
+	
+	var client = sjr.client({
+		call: function (method, args, cb) {
+			cb(null, { method: method, args: args });
+		}
+	});
+	
+	client.call('method1', ['arg1', 'arg2'], function(err, data) {
+		test.equal(err, null);
+		test.ok(data);
+		test.deepEqual(data, { method: 'method1', args: [ 'arg1', 'arg2' ]});
+		test.done();
+	});
+}
+
